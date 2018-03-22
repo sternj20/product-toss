c00c080cec6d6427a3ab0a52ae4d007d38097c9d
sternj20 <jessesternmusic@gmail.com>
2018-03-21 18:32:16 -0700

    added footer with ionic icons

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -6,6 +6,10 @@ import { LoadingIndicator } from '../loadingIndicator/loadingIndicator';
 import  VotingBar  from '../votingBar/votingBar';
 import  VotingGallery  from '../votingGallery/votingGallery';
 import { ImagePicker, FileSystem } from 'expo'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+
+
 
 export class Home extends React.Component {
   componentDidMount() {
@@ -44,17 +48,24 @@ export class Home extends React.Component {
     const { loading } = this.props;
 
     return (
-      <View >
-
-        {loading ? <LoadingIndicator color="#ffffff"
-                                       size="large"/> : <View >
-              <Text style={styles.title}>Welcome {this.props.user.email}</Text>
-              <Button onPress={this.logout.bind(this)} title="Logout"></Button>
-              <Button onPress={this.showImages} title="Your Images"/>
-              <Button onPress={this.createContest} title="Create Contest"/>
-              <Button onPress={this.choosePhoto.bind(this)} title="Upload Photo"/>
-            <VotingGallery/></View>}
-      </View>
+        <View >
+            {loading ? <LoadingIndicator color="#ffffff" size="large"/> : 
+            <View >
+                <Text style={styles.title}>Welcome {this.props.user.email}</Text>
+                <Button onPress={this.logout.bind(this)} title="Logout"></Button>
+                <Button onPress={this.showImages} title="Your Images"/>
+                <Button onPress={this.createContest} title="Create Contest"/>
+                <Button onPress={this.choosePhoto.bind(this)} title="Upload Photo"/>
+                <VotingGallery/>
+                <View style={{ height: 54, backgroundColor: 'white', flexDirection: 'row'}}>
+                  <Ionicons name="ios-home" size={34} color="black" style={{ marginTop: 12, marginLeft: 15 }} />
+                  <Ionicons name="ios-search" size={34} color="black" style={{ marginTop: 12, marginLeft: 20 }} />
+                  <FontAwesome name="diamond" size={34} color="blue" style={{ marginTop: 12, marginLeft: 20 }} />
+                  <Ionicons onPress={this.choosePhoto.bind(this)} name="ios-camera" size={34} color="black" style={{ marginTop: 12, marginLeft: 20 }} />
+                  <Ionicons name="ios-chatbubbles" size={34} color="black" style={{ marginTop: 12, marginLeft: 20 }} />
+                </View>
+            </View>}
+        </View>
       );
   }
 }
