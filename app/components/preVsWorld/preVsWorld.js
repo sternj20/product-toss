59548ade9ba15d2ca54d5921dfaf0eccd23729e9
sternj20 <jessesternmusic@gmail.com>
2018-03-31 11:34:04 -0700

    additional conditional rendering in prevsworld component

--- app/components/preVsWorld/preVsWorld.js
+++ app/components/preVsWorld/preVsWorld.js
@@ -3,7 +3,7 @@ import { View, Button, Image, Text, TouchableOpacity, ScrollView } from 'react-n
 import { chooseContestToSee } from '../../actions/vote/vote'
 import { connect } from 'react-redux';
 import { styles } from './styles';
-import { itemsFetchData, showSingleImageFromOther} from '../../actions/items/items';
+import { itemsFetchData, showSingleImageFromOther, showSingleImage} from '../../actions/items/items';
 
 
 class preVsWorld extends React.Component {
@@ -20,7 +20,7 @@ class preVsWorld extends React.Component {
                         uid: submission.createdBy
                     }
                     return(
-                    <TouchableOpacity onPress={() => this.props.showSingleImageFromOther(submission, this.props.navigation, user)} key={`${index}Container`} style={styles.container} >
+                    <TouchableOpacity onPress={() => user.uid ? this.props.showSingleImageFromOther(submission, this.props.navigation, user) : this.props.showSingleImage(submission, this.props.navigation, this.props.user)} key={`${index}Container`} style={styles.container} >
                     <Image 
                     source={{uri:submission.url}} 
                     key={submission._id} 
@@ -42,7 +42,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps =  {
     fetchData: itemsFetchData,
-    showSingleImageFromOther
+    showSingleImageFromOther,
+    showSingleImage
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(preVsWorld);
