d65b3fa5c3256431b489ae08151d36664aa62d69
sternj20 <jessesternmusic@gmail.com>
2018-04-18 23:39:04 -0700

    added comment layout to single image component and added some dummy comments

--- app/components/singleImage/singleImage.js
+++ app/components/singleImage/singleImage.js
@@ -1,83 +1,120 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { View, Button, Image, Text, TouchableOpacity } from 'react-native';
-import { styles } from './styles';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
-import { deleteImage} from '../../actions/items/items'
-import {seeFriendsData} from "../../actions/social/social"
-import {navigateToComponent} from "../../utils/helpers.js"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { View, Button, Image, Text, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { deleteImage } from "../../actions/items/items";
+import { seeFriendsData } from "../../actions/social/social";
+import { navigateToComponent } from "../../utils/helpers.js";
 
 class singleImage extends Component {
-
     componentDidMount() {
-
         this.props.navigation.setParams({
-            singleImage: this.props.singleImage,
+            singleImage: this.props.singleImage
         });
     }
 
-    //USE THIS IN PREVSWORLD SOMEHOW 
-    static navigationOptions = ({navigation}) => {
-        const navigationHelper = (params) => {
-            if (params.otherUser){
-                navigation.dispatch(seeFriendsData(navigation, params.otherUser))
+    //USE THIS IN PREVSWORLD SOMEHOW
+    static navigationOptions = ({ navigation }) => {
+        const navigationHelper = params => {
+            if (params.otherUser) {
+                navigation.dispatch(
+                    seeFriendsData(navigation, params.otherUser)
+                );
             } else {
-                navigateToComponent(navigation, params.user, 'userImages')
+                navigateToComponent(navigation, params.user, "userImages");
             }
-    }
-    const { params } = navigation.state;
-    return{
-        headerStyle: {
-            backgroundColor: "#f4511e",
-            height: 70
-        },
-        headerTitle: () => (
-          <View style={styles.headerWrapper}>
-            <TouchableOpacity onPress={()=>navigationHelper(params)}>
-            <Text
-              adjustsFontSizeToFit
-              style={styles.headerText}>{params.otherUser ? params.otherUser.userName : params.user.email.split('@')[0] }</Text>
-            </TouchableOpacity>
-          </View>
-        ),
-        headerRight: (
-            <View style={styles.headerWrapper}>
-                <TouchableOpacity
-                    onPress={() =>
-                        navigation.navigate('Home')
-                    }
-                    style={{ flexDirection: "row" }}
-                >
-                    <MaterialIcons name="home" size={50} color="white" />
-                </TouchableOpacity>
-            </View>
-        )
-    }
-}
-
-    render(){
-        return(
-            <View>
-                <View>
-                    <Image 
-                    source={{uri:this.props.singleImage.url}} 
-                    style={styles.image}/>
+        };
+        const { params } = navigation.state;
+        return {
+            headerStyle: {
+                backgroundColor: "#f4511e",
+                height: 70
+            },
+            headerTitle: () => (
+                <View style={styles.headerWrapper}>
+                    <TouchableOpacity onPress={() => navigationHelper(params)}>
+                        <Text adjustsFontSizeToFit style={styles.headerText}>
+                            {params.otherUser
+                                ? params.otherUser.userName
+                                : params.user.email.split("@")[0]}
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-                <Button onPress={() => deleteImage(this.props.user.uid, this.props.singleImage.url, this.props.singleImage._id)}  title="delete"/>
+            ),
+            headerRight: (
+                <View style={styles.headerWrapper}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Home")}
+                        style={{ flexDirection: "row" }}
+                    >
+                        <MaterialIcons name="home" size={50} color="white" />
+                    </TouchableOpacity>
+                </View>
+            )
+        };
+    };
 
+    render() {
+        return (
+            <View>
+                <Image
+                    source={{ uri: this.props.singleImage.url }}
+                    style={styles.image}
+                />
+                <View style={styles.singleImageHeader}>
+                    <FontAwesome name="diamond" color="teal" size={20} />
+                    <Text style={styles.singleImageHeadText}>00</Text>
+                    <View style={styles.bars}>
+                        <FontAwesome name="bars" color="#f4511e" size={40} />
+                    </View>
+                </View>
+                <View style={styles.comments}>
+                    <View>
+                        <Ionicons
+                            name="ios-chatbubbles"
+                            size={30}
+                            color="teal"
+                        />
+                    </View>
+                    <View style={styles.commentContent}>
+                        <View style={styles.comment}>
+                            <Text style={styles.commentUser}>username</Text>
+                            <Text style={styles.commentText}>Newest comments show up first in this list.</Text>
+                        </View>
+                        <View style={styles.comment}>
+                            <Text style={styles.commentUser}>username</Text>
+                            <Text style={styles.commentText}>older comments move down the list as it builds</Text>
+                        </View>
+                        <View style={styles.comment}>
+                            <Text style={styles.commentUser}>username</Text>
+                            <Text style={styles.commentText}>when the list gets too long it loads as a user scrolls down</Text>
+                        </View>
+                    </View>
+                </View>
             </View>
-        )
+        );
     }
 }
-
 
 const mapStateToProps = state => ({
     singleImage: state.itemReducer.singleImage,
     user: state.sessionReducer.user
-})
+});
 
-const mapDispatchToProps = {
-}
+const mapDispatchToProps = {};
 
+export default connect(mapStateToProps, mapDispatchToProps)(singleImage);
 
-export default connect(mapStateToProps, mapDispatchToProps)(singleImage)
+// <Button
+//     onPress={() =>
+//         deleteImage(
+//             this.props.user.uid,
+//             this.props.singleImage.url,
+//             this.props.singleImage._id
+//         )
+//     }
+//     title="delete"
+// />
--- app/components/singleImage/styles.js
+++ app/components/singleImage/styles.js
@@ -1,33 +1,69 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  image: {
-    width: '100%',
-    height: 400,
+    image: {
+        width: "100%",
+        height: "65%"
+    },
+    button: {
+        backgroundColor: "#88cc88",
+        height: 40,
+        margin: 10,
+        borderRadius: 5,
+        padding: 3,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    user: {
+        flexDirection: "row"
+    },
+    userHeader: {
+        fontSize: 30
+    },
+    singleImageHeader: {
+        flexDirection: "row"
+    },
+    singleImageHeadText: {
+        fontSize: 20,
+        color: 'teal',
+        fontWeight: 'bold',
+        marginLeft: 10
+    },
+    headerWrapper: {
+        flex: 1
+    },
+    commentContent: {
+        marginLeft: 20,
+    },
+    commentUser: {
+        fontSize: 20,
+        color: 'teal',
+        fontWeight: 'bold',
+    },
+    comments: {
+        flexDirection: 'row'
+    },
+    comment: {
+        flexDirection: 'row',
+        flexWrap:'wrap',
+
+    },
+    commentText: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        marginLeft: 10
+    },
+    headerText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: "center", // ok
+        alignSelf: "center" // ok
+    },
+  bars: {
+    position: 'absolute',
+    right: 10,
+    alignItems: 'flex-end',
+    width: '22%',
   },
-  button: {
-    backgroundColor: '#88cc88',
-    height: 40,
-    margin: 10,
-    borderRadius: 5,
-    padding: 3,
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  user:{
-    flexDirection: 'row'
-  },
-  userHeader:{
-    fontSize:30
-  },
-headerWrapper: {
-    flex: 1,
-  },
-  headerText: {
-    fontSize:40,
-    fontWeight:'bold',
-    color: 'white',
-    textAlign: 'center', // ok
-    alignSelf: 'center', // ok
-  }
 });
